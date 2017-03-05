47de4d171ab72d939a516db5f42452d3d34638d1
Brennan Wallace <brennanwallace@dyn-160-39-135-80.dyn.columbia.edu>
2017-03-05 00:52:00 -0500

    working mvp with one sentence diff

--- js/main.js
+++ js/main.js
@@ -109,7 +109,6 @@ for (var key in data) {
     var ctx = $("#" + key).get(0).getContext("2d");
     var activeChart = new Chart(ctx, data[key]);
     $('canvas').hide();
-    $('#' + 1).show();
 }
 
 
@@ -121,13 +120,22 @@ for (var i = 1; i < 5; i++) {
     }
 }
 var currentDealership = 1;
+var firstSelection = true;
 
 $( document ).ready(function() {
     $( ".dealershipSelector" ).click(function() {
+
         var newId = Number($(this)[0]['dataset'].dealershipId);
         $('h1').text("Dealership " + newId);
         $('canvas').hide();
         $('#' + newId).show();
+
+        if (firstSelection) {
+            currentDealership = newId;
+            firstSelection = false;
+            return;
+        }
+
         transitions[currentDealership][newId] += 1;
         var contextDict = {};
 
@@ -164,16 +172,41 @@ $( document ).ready(function() {
 
         contextArr.reverse();
 
-        var mostReleventComparison = contextArr[0].id;
-
-        currentDealership = newId;
-
+        var mostRelevantComparison = contextArr[0].id;
 
         //compare the two data sets.
+        //mostReleventComparison and newID
 
 
+        var biggestDiffDataIndex = {
+            'index':0,
+            'absDiff': Math.abs(data[newId].data.datasets[0].data[0] - data[mostRelevantComparison].data.datasets[0].data[0]),
+            'diff' : data[newId].data.datasets[0].data[0] - data[mostRelevantComparison].data.datasets[0].data[0]
+        };
 
+        for (let k = 1; k < data[newId].data.datasets[0].data.length; k++) {
+            const diff = data[newId].data.datasets[0].data[k] - data[mostRelevantComparison].data.datasets[0].data[k];
+            const absDiff = Math.abs(diff);
+            if (absDiff > biggestDiffDataIndex.absDiff) {
+                biggestDiffDataIndex.index = k;
+                biggestDiffDataIndex.absDiff = absDiff;
+                biggestDiffDataIndex.diff = diff;
+            }
+        }
 
+        if (biggestDiffDataIndex.absDiff < 12) {
+            var string = "Dealership "+ newId + " and dealership " + currentDealership + " seem about the same."
+        } else if (biggestDiffDataIndex.diff < 0) {
+            var string = ("Dealership "+ newId + " has a lot less sales for " +
+                data[4].data.labels[biggestDiffDataIndex.index] + "s than dealership " + currentDealership);
+        } else {
+            var string = ("Dealership "+ newId + " has a lot more sales for " +
+            data[4].data.labels[biggestDiffDataIndex.index] + "s than dealership " + currentDealership);
+        }
+
+        $("#contextMessage").text(string);
+
+        currentDealership = newId;
     });
 
     $( "#TTT" ).click(function() {
